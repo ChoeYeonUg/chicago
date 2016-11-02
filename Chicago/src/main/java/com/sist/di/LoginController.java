7aa75b4d3a5a84904a1c4152b51a6536aa92564e
twi_dawn <fear_death@daum.net>
2016-11-02 12:13:18 +0900

    add aop

--- Chicago/src/main/java/com/sist/di/LoginController.java
+++ Chicago/src/main/java/com/sist/di/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
@@ -21,6 +22,7 @@ import com.sist.dao.loginVO;
 import com.sist.service.MemberService;
 
 @Controller
+@EnableAspectJAutoProxy
 public class LoginController {
 	/* private */
 	
